48f5669b3b91fe4b00b80148ee43e8fbafc924bb
Rubaiyat E Mohammad <rubaiyat.e.mohammad@g.bracu.ac.bd>
2023-08-22 23:44:41 +0600

    Second Commit

--- src/test/java/Selenium/TestNGannotation.java
+++ src/test/java/Selenium/TestNGannotation.java
@@ -18,45 +18,45 @@ public class TestNGannotation {
 		driver.get("https://www.saucedemo.com/");
 	}
 	
-	@Test(priority = 0)
+	@Test(priority = 1)
 	public void urlCheck() {
 		String ExpectedURL = "https://www.saucedemo.com/";
 		String ActualURL = driver.getCurrentUrl();
 		Assert.assertEquals(ActualURL, ExpectedURL, "URL Doesn't Matched");
 	}
 	
-	@Test(priority = 1)
+	@Test(priority = 2)
 	public void titleCheck() {
 		String ExpectedTitle = "Swag Labs";
 		String ActualTitle = driver.getTitle();
 		Assert.assertEquals(ActualTitle, ExpectedTitle, "Title Doesn't Matched");
 	}
 	
-	@Test(priority = 2)
+	@Test(priority = 3)
 	public void loggingIn() {
 		driver.findElement(By.id("user-name")).sendKeys("standard_user");
 		driver.findElement(By.id("password")).sendKeys("secret_sauce");
 		driver.findElement(By.id("login-button")).click();
 	}
 	
-	@Test(priority = 3)
+	@Test(priority = 4)
 	public void addRemoveCart() {
 		driver.findElement(By.xpath("//button[@id='add-to-cart-sauce-labs-backpack']")).click();
 		driver.findElement(By.xpath("//button[@id='remove-sauce-labs-backpack']")).click();
 	}
 	
-	@Test(priority = 4)
+	@Test(priority = 5)
 	public void openCart() {
 		driver.findElement(By.cssSelector("button#add-to-cart-sauce-labs-backpack")).click(); 
 		driver.findElement(By.cssSelector("#shopping_cart_container > a > span")).click();
 	}
 	
-	@Test(priority = 5)
+	@Test(priority = 6)
 	public void checkOut() {
 		driver.findElement(By.className("checkout_button")).click();
 	}
 	
-	@Test(priority = 6)
+	@Test(priority = 7)
 	public void orderInfo() {
 		driver.findElement(By.xpath("/html/body/div/div/div/div[2]/div/form/div[1]/div[1]/input")).sendKeys("Rubaiyat E");
 		driver.findElement(By.xpath("/html/body/div/div/div/div[2]/div/form/div[1]/div[2]/input")).sendKeys("Mohammad");
@@ -64,7 +64,7 @@ public class TestNGannotation {
 		driver.findElement(By.xpath("/html/body/div/div/div/div[2]/div/form/div[2]/input")).click();
 	}
 	
-	@Test(priority = 7)
+	@Test(priority = 8)
 	public void finishToHome() {
 		driver.findElement(By.xpath("/html/body/div/div/div/div[2]/div/div[2]/div[9]/button[2]")).click();
 		driver.findElement(By.xpath("/html/body/div/div/div/div[2]/button")).click();
